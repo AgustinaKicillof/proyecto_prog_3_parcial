589641d11410f037ae2108f6bd5efb2fa58ee789
horasbarbeito <hbarbeito@udesa.edu.ar>
2022-04-09 13:06:53 -0300

    Css primera parte

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import Header from "../Header/Header"
-import Track from "../Track/Track"
+import Header from "../Header/Header";
+import Track from "../Track/Track";
+import './main.css'
 
 class Main extends Component {
   constructor() {
